90298648e0d344df8d858b1c7c3b06ce389acf9c
Yuri Brunetto <brunettoyuri@gmail.com>
2019-12-04 13:02:36 -0300

    remove netlify stuff

--- server-local.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-const app = require('./express/server')
-
-app.listen(8888, () => console.log('Local app listening on port 8888!'))
--- server.js
+++ server.js
@@ -1,31 +1,25 @@
-require('dotenv').config()
-
-const serverless = require('serverless-http')
-const path = require('path')
 const express = require('express')
 const request = require('request')
 const querystring = require('querystring')
-const bodyParser = require('body-parser')
 
 const app = express()
 
-const router = express.Router()
+const redirect_uri =
+  process.env.REDIRECT_URI || 'http://localhost:8888/callback'
 
-let redirect_uri = process.env.REDIRECT_URI || 'http://localhost:8888/callback'
-
-router.get('/login', function(req, res) {
+app.get('/login', function(req, res) {
   res.redirect(
     'https://accounts.spotify.com/authorize?' +
       querystring.stringify({
         response_type: 'code',
         client_id: process.env.SPOTIFY_CLIENT_ID,
-        scope: 'user-read-private user-read-email user-top-read',
+        scope: 'user-read-private user-read-email',
         redirect_uri
       })
   )
 })
 
-router.get('/callback', function(req, res) {
+app.get('/callback', function(req, res) {
   let code = req.query.code || null
   let authOptions = {
     url: 'https://accounts.spotify.com/api/token',
@@ -47,14 +41,13 @@ router.get('/callback', function(req, res) {
   }
   request.post(authOptions, function(error, response, body) {
     var access_token = body.access_token
-    let uri = process.env.FRONTEND_URI || 'http://localhost:3000/tracks'
+    let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
     res.redirect(uri + '?access_token=' + access_token)
   })
 })
 
-app.use(bodyParser.json())
-app.use('/.netlify/functions/server', router) // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')))
-
-module.exports = app
-module.exports.handler = serverless(app)
+let port = process.env.PORT || 8888
+console.log(
+  `Listening on port ${port}. Go /login to initiate authentication flow.`
+)
+app.listen(port)
